e803cc4767e482dcf987c835209afac5c397fa3a
Gramms Cooper <87103634+GrammsLHS@users.noreply.github.com>
2022-02-09 22:08:30 +0800

    10.08

--- Booking.c
+++ Booking.c
@@ -8,7 +8,7 @@
 void test();
 //Hau Shien is here once again 9.37pm  9.41pm
 //CY is here once again 9:48pm
-//push and pull, here we go
+//push and pull, here we go 10.08pm
 typedef struct {
 	char bookID[6];
 	char status[11];
